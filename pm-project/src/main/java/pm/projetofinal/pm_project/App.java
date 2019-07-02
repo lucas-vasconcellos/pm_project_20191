877be93b058183a59b4a2499d6cc04cd6678daf3
brouck <jgbrouck@gmail.com>
2019-07-02 13:22:30 -0300

    Refactor

--- pm-project/src/main/java/pm/projetofinal/pm_project/App.java
+++ pm-project/src/main/java/pm/projetofinal/pm_project/App.java
@@ -1,72 +1,88 @@
 package pm.projetofinal.pm_project;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import pm.projetofinal.pm_project.Model.*;
-import pm.projetofinal.pm_project.Service.*;
+import pm.projetofinal.pm_project.Model.BoundingBox;
+import pm.projetofinal.pm_project.Model.Municipio;
+import pm.projetofinal.pm_project.Service.BoundingBoxService;
+import pm.projetofinal.pm_project.Service.DomService;
+import pm.projetofinal.pm_project.Service.MunicipioService;
+import pm.projetofinal.pm_project.Service.OverpassService;
 import pm.projetofinal.pm_project.Utils.Utils;
-import pm.projetofinal.pm_project.Controller.*;
 
-import com.opencsv.*;
+public class App
+{
 
-public class App {
 	static BoundingBoxService boundingBoxService = new BoundingBoxService();
-	static MunicipioService municipioService = new MunicipioService();
+
 	static DomService domService = new DomService();
-	static OverpassService overpassService =  new OverpassService();
 
 	public static final String MUNICIPIO_KML_TAG = "Placemark";
 
-	public static void main(String[] args) throws ParserConfigurationException, SAXException {
+	static MunicipioService municipioService = new MunicipioService();
+
+	static OverpassService overpassService = new OverpassService();
+
+	public static void main( final String[] args )
+		throws ParserConfigurationException,
+			SAXException
+	{
 
 		String uf = "";
 		boolean isCode = true;
 		String entryMunicipio = "";
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		final BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
 
-		try {
+		try
+		{
+
 			entryMunicipio = reader.readLine().toString().toUpperCase();
 
-			if (Utils.isIntegerParseInt(entryMunicipio)) {
-				uf = entryMunicipio.substring(0, 2);
-				uf = Utils.mapaUF.get(entryMunicipio.substring(0, 2));
-			} else {
-				uf = entryMunicipio.split("-")[1];
+			if ( Utils.isIntegerParseInt( entryMunicipio ) )
+			{
+				uf = entryMunicipio.substring( 0, 2 );
+				uf = Utils.UF_MAP.get( entryMunicipio.substring( 0, 2 ) );
+			}
+			else
+			{
+				uf = entryMunicipio.split( "-" )[1];
 				isCode = false;
 			}
-		} catch (IOException e1) {
-			System.out.println("Informação inválida foi inserida.");
+		}
+		catch ( final IOException e1 )
+		{
+			System.out.println( "Informação inválida foi inserida." );
 		}
 
-		String filePath = String.format("src\\kml\\%s.kml", uf);
-		File xmlFile = new File(filePath);
+		final String filePath = String.format( "src\\kml\\%s.kml", uf );
+		final File xmlFile = new File( filePath );
 
-		try {
-			NodeList nodeList = domService.getNodeListFromFile(xmlFile, MUNICIPIO_KML_TAG);
-			Municipio municipio = municipioService.getMunicipioFromNodeList(nodeList, entryMunicipio, isCode);
-			BoundingBox boundingBox = municipio.getBoundingBox();
-			Document overPassResponse = overpassService.getOverpassDocument(boundingBox);
+		try
+		{
 
-			//TODO utilizar o document para buscar os dados e montar a exibição.
+			final NodeList nodeList = domService.getNodeListFromFile( xmlFile, MUNICIPIO_KML_TAG );
+			final Municipio municipio = municipioService.getMunicipioFromNodeList( nodeList, entryMunicipio, isCode );
+			final BoundingBox boundingBox = municipio.getBoundingBox();
+			final Document overPassResponse = overpassService.getOverpassDocument( boundingBox );
+
+			// TODO utilizar o document para buscar os dados e montar a exibição.
+			// https://wiki.openstreetmap.org/wiki/Map_Features#Highway
 			// NodeList nodeList2 = is.getElementsByTagName("way");
 			// System.out.println(nodeList2.getLength());
 
-		} catch (IOException e) {
-			System.out.println("Houve um problema com a requisição.");
+		}
+		catch ( final IOException e )
+		{
+			System.out.println( "Houve um problema com a requisição." );
 		}
 
 	}
--- pm-project/src/main/java/pm/projetofinal/pm_project/Utils/Utils.java
+++ pm-project/src/main/java/pm/projetofinal/pm_project/Utils/Utils.java
@@ -3,49 +3,55 @@ package pm.projetofinal.pm_project.Utils;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Utils {
+public class Utils
+{
 
-	public static final Map<String, String> mapaUF = new HashMap<String, String>();
+	public static final Map<String, String> UF_MAP = new HashMap<String, String>();
 
-	static {
-		mapaUF.put(new String("12"), "AC");
-		mapaUF.put(new String("27"), "AL");
-		mapaUF.put(new String("13"), "AM");
-		mapaUF.put(new String("12"), "AP");
-		mapaUF.put(new String("12"), "BA");
-		mapaUF.put(new String("12"), "CE");
-		mapaUF.put(new String("16"), "AP");
-		mapaUF.put(new String("29"), "BA");
-		mapaUF.put(new String("23"), "CE");
-		mapaUF.put(new String("53"), "DF");
-		mapaUF.put(new String("32"), "ES");
-		mapaUF.put(new String("52"), "GO");
-		mapaUF.put(new String("21"), "MA");
-		mapaUF.put(new String("31"), "MG");
-		mapaUF.put(new String("50"), "MS");
-		mapaUF.put(new String("51"), "MT");
-		mapaUF.put(new String("15"), "PA");
-		mapaUF.put(new String("25"),"PB");
-		mapaUF.put(new String("26"), "PE");
-		mapaUF.put(new String("22"), "PI");
-		mapaUF.put(new String("41"), "PR");
-		mapaUF.put(new String("33"), "RJ");
-		mapaUF.put(new String("24"), "RN");
-		mapaUF.put(new String("11"), "RO");
-		mapaUF.put(new String("14"), "RR");
-		mapaUF.put(new String("43"), "RS");
-		mapaUF.put(new String("42"), "SC");
-		mapaUF.put(new String("28"), "SE");
-		mapaUF.put(new String("35"), "SP");
-		mapaUF.put(new String("17"), "TO");
-	}
-
-	public static boolean isIntegerParseInt(String str) {
-		try {
-			Integer.parseInt(str);
+	public static boolean isIntegerParseInt( final String str )
+	{
+		try
+		{
+			Integer.parseInt( str );
 			return true;
-		} catch (NumberFormatException nfe) {
+		}
+		catch ( final NumberFormatException nfe )
+		{
 		}
 		return false;
+	}
+
+	static
+	{
+		UF_MAP.put( new String( "12" ), "AC" );
+		UF_MAP.put( new String( "27" ), "AL" );
+		UF_MAP.put( new String( "13" ), "AM" );
+		UF_MAP.put( new String( "12" ), "AP" );
+		UF_MAP.put( new String( "12" ), "BA" );
+		UF_MAP.put( new String( "12" ), "CE" );
+		UF_MAP.put( new String( "16" ), "AP" );
+		UF_MAP.put( new String( "29" ), "BA" );
+		UF_MAP.put( new String( "23" ), "CE" );
+		UF_MAP.put( new String( "53" ), "DF" );
+		UF_MAP.put( new String( "32" ), "ES" );
+		UF_MAP.put( new String( "52" ), "GO" );
+		UF_MAP.put( new String( "21" ), "MA" );
+		UF_MAP.put( new String( "31" ), "MG" );
+		UF_MAP.put( new String( "50" ), "MS" );
+		UF_MAP.put( new String( "51" ), "MT" );
+		UF_MAP.put( new String( "15" ), "PA" );
+		UF_MAP.put( new String( "25" ), "PB" );
+		UF_MAP.put( new String( "26" ), "PE" );
+		UF_MAP.put( new String( "22" ), "PI" );
+		UF_MAP.put( new String( "41" ), "PR" );
+		UF_MAP.put( new String( "33" ), "RJ" );
+		UF_MAP.put( new String( "24" ), "RN" );
+		UF_MAP.put( new String( "11" ), "RO" );
+		UF_MAP.put( new String( "14" ), "RR" );
+		UF_MAP.put( new String( "43" ), "RS" );
+		UF_MAP.put( new String( "42" ), "SC" );
+		UF_MAP.put( new String( "28" ), "SE" );
+		UF_MAP.put( new String( "35" ), "SP" );
+		UF_MAP.put( new String( "17" ), "TO" );
 	}
 }

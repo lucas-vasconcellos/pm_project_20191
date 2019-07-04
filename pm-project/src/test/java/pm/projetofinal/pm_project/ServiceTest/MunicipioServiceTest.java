bfc8caa64e8fc71c46fcfd3655494a46479e6044
Lucas Carvalho <lcarvalho94@hotmail.com>
2019-07-04 00:27:16 -0300

    Adicionado a classe responsável pelos testes da classe MunicipioService

---
+++ pm-project/src/test/java/pm/projetofinal/pm_project/ServiceTest/MunicipioServiceTest.java
@@ -0,0 +1,72 @@
+package pm.projetofinal.pm_project.ServiceTest;
+
+import static org.mockito.Mockito.*;
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import pm.projetofinal.pm_project.Model.Municipio;
+import pm.projetofinal.pm_project.Service.DomService;
+import pm.projetofinal.pm_project.Service.MunicipioService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
+@RunWith(PowerMockRunner.class)
+public class MunicipioServiceTest {
+
+	MunicipioService municipioService = new MunicipioService();
+	DomService domService = new DomService();
+
+	@Test
+	public void testGetMunicipioFromNodeListExists() throws ParserConfigurationException, SAXException, IOException
+	{
+		final String MUNICIPIO_CODIGO = "3300605";
+		final boolean isCode = true;
+		final String MUNICIPIO_KML_TAG = "Placemark";
+		final String filePath = String.format( "src\\kml\\RJ.kml");
+		final File xmlFile = new File(filePath);
+		
+		NodeList nodeList = domService.getNodeListFromFile( xmlFile, MUNICIPIO_KML_TAG );
+		Municipio output = municipioService.getMunicipioFromNodeList(nodeList, MUNICIPIO_CODIGO, isCode);
+		
+		boolean isInstanceOfMunicipio = output instanceof Municipio;
+        boolean resultadoEsperado = true;    
+        Assert.assertEquals(resultadoEsperado, isInstanceOfMunicipio); 
+	}
+	
+	//@PrepareForTest(System.class)
+	//@Test
+	//public void testGetMunicipioFromNodeListNotExists() throws ParserConfigurationException, SAXException, IOException
+	//{
+		//MunicipioService municipioService = new MunicipioService();
+		//DomService domService = new DomService();
+		
+		//final String MUNICIPIO_CODIGO = "3300605";
+		//final boolean isCode = true;
+		//final String MUNICIPIO_KML_TAG = "Placemark";
+		//final String filePath = String.format( "src\\kml\\ES.kml");
+		//final File xmlFile = new File(filePath);
+		
+		//NodeList nodeList = domService.getNodeListFromFile( xmlFile, MUNICIPIO_KML_TAG );
+		
+		//PowerMockito.mockStatic(System.class);
+		//String mockReturn = PowerMockito.when(System.out.println()).thenReturn("System.exit(0) was called!");
+		
+		//boolean resultadoEsperado = true;
+		//Assert.assertEquals(resultadoEsperado, municipioService.getMunicipioFromNodeList(nodeList, MUNICIPIO_CODIGO, isCode));;
+	//}
+	
+	@Test
+	public void testGetMunicipio()
+	{
+		
+	}
+}

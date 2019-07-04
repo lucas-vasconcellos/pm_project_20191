172275ab6b945130387f9ae38ac95fc8e456abcb
Lucas Carvalho <lcarvalho94@hotmail.com>
2019-07-03 21:40:11 -0300

    Adicionado mais um teste na classe OverpassApiControllerTest Alterado o junit para a versao 4.12

--- pm-project/src/test/java/pm/projetofinal/pm_project/ControllerTest/OverpassApiControllerTest.java
+++ pm-project/src/test/java/pm/projetofinal/pm_project/ControllerTest/OverpassApiControllerTest.java
@@ -1,19 +1,29 @@
 package pm.projetofinal.pm_project.ControllerTest;
-import junit.framework.*;
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 import pm.projetofinal.pm_project.Controller.OverpassApiController;
+import org.junit.*;
 
-public class OverpassApiControllerTest extends TestCase{
+public class OverpassApiControllerTest {
 	
-	public void testGetOverpassApiXml() throws IOException, SAXException, ParserConfigurationException
+	@Test
+	public void testGetOverpassApiXmlSuccess() throws IOException, SAXException, ParserConfigurationException
     {
-        String parametroTest = "-42.18248958899994,-22.93313082199995,-41.88901108899994,-22.54012577499998";
-        Document output = OverpassApiController.getOverpassApiXml(parametroTest);
-        boolean isInstanceOfDocument = output instanceof Document;
+        final String parametroTest = "-42.18248958899994,-22.93313082199995,-41.88901108899994,-22.54012577499998";
         
-        assertEquals(isInstanceOfDocument, true);    
+        Document output = OverpassApiController.getOverpassApiXml(parametroTest);
+        
+        boolean isInstanceOfDocument = output instanceof Document;
+        boolean resultadoEsperado = true;    
+        Assert.assertEquals(isInstanceOfDocument, resultadoEsperado);    
+    }
+	
+	@Test(expected = IOException.class)
+	public void testGetOverpassApiXmlFailure() throws IOException, SAXException, ParserConfigurationException
+    {
+		final String parametroTest = "abcde";
+		Document output = OverpassApiController.getOverpassApiXml(parametroTest);
     }
 }
